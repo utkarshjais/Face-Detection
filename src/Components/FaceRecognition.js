ef0e183cbf2eca5220a7d63aaef0b8ed70f577e9
utkarshjais <erutkarshjaiswal@gmail.com>
2020-09-01 15:17:27 +0530

    Added Face Detection css and box'

--- src/Components/FaceRecognition.js
+++ src/Components/FaceRecognition.js
@@ -1,8 +1,10 @@
 import React from 'react';
-const FaceRecognition=({imageUrl})=>{
+import './FaceRecognition.css';
+const FaceRecognition=({box,imageUrl})=>{
 	return(
-		<div className='center mt2 '>
-		<img alt=''src={imageUrl}width='500px'height='auto'/>
+		<div className='center absolute mt2'>
+		<img id='inputImage'alt=''src={imageUrl}width='500px'height='auto'/>
+		<div className='bounding-box'style={{top:box.topRow,right:box.rightCol,bottom:box.bottomRow,left:box.leftCol}}></div>
 		</div>
 		);
 }
